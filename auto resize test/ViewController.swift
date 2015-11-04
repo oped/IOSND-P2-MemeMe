919d5746fe5d8b28c2240ce2971e0fd031933e7d
Jay <jay@zikursh.com>
2015-11-03 19:52:43 -0500

    Initial Commit

---
+++ auto resize test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto resize test
+//
+//  Created by josh peterson on 11/3/15.
+//  Copyright © 2015 Zikursh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
